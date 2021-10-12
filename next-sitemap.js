a81b45af95272b0e114e744c9a6342dc2b48ac3b
jakobkarlstrand <jakobkarlstrand@gmail.com>
2021-10-12 13:36:38 +0200

    Sitemap added

---
+++ next-sitemap.js
@@ -0,0 +1,39 @@
+module.exports = {
+    siteUrl: 'https://medieteknikdagen.se',
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+    generateRobotsTxt: true,
+    alternateRefs: [
+      {
+        href: 'https://medieteknikdagarna.se'
+      },
+      {
+        href: 'https://mediatechnologydays.com/'
+      },
+    ],
+    // Default transformation function
+    transform: async (config, path) => {
+      return {
+        loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+        changefreq: config.changefreq,
+        priority: config.priority,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+      }
+    },
+    additionalPaths: async (config) => [
+      await config.transform(config, '/additional-page'),
+    ],
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+      additionalSitemaps: [
+        'https://medieteknikdagen.se/sitemap.xml'
+      ],
+    },
+  }
