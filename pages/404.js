28bc3d9ebe51a38bc66e8fb01815b7142cb86212
jakobkarlstrand <jakobkarlstrand@gmail.com>
2021-10-31 12:51:04 +0100

    Redirect 404

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,12 @@
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+
 export default function Custom404() {
-    return null;
-  }
-  
-  export const getStaticProps = () => {
-    return {
-      redirect: {
-        destination: '/',
-      },
-    };
-  };
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace("/")
+  })
+
+  return null
+}
