a7560227951e2d2c6a119dd2ba913fc1cb7f7d2a
Dennis <dennis.moradkhani@gmail.com>
2022-09-13 13:48:44 +0200

    replace string

--- pages/api/register.js
+++ pages/api/register.js
@@ -9,9 +9,13 @@ const creds = {
   client_id: process.env.GOOGLE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  auth_provider_x509_cert_url:
+    "https://www.googleapis.com/oauth2/v1/certs"?.replace(/\\n/gm, "\n"),
   client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/spreadsheet%40medieteknikdagen.iam.gserviceaccount.com",
+    "https://www.googleapis.com/robot/v1/metadata/x509/spreadsheet%40medieteknikdagen.iam.gserviceaccount.com"?.replace(
+      /\\n/gm,
+      "\n"
+    ),
 };
 
 export async function addRegistration(data) {
