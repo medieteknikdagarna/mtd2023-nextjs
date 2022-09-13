8368a17c9edee7d7ff800863861b2f308a266c4c
Dennis <dennis.moradkhani@gmail.com>
2022-09-13 13:53:20 +0200

    more string replace

--- pages/api/register.js
+++ pages/api/register.js
@@ -7,8 +7,8 @@ const creds = {
   private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
   client_id: process.env.GOOGLE_CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth"?.replace(/\\n/gm, "\n"),
+  token_uri: "https://oauth2.googleapis.com/token"?.replace(/\\n/gm, "\n"),
   auth_provider_x509_cert_url:
     "https://www.googleapis.com/oauth2/v1/certs"?.replace(/\\n/gm, "\n"),
   client_x509_cert_url:
