392ae352520a9772cb228f99cdb88a85177d2eb7
Dennis <dennis.moradkhani@gmail.com>
2022-11-09 22:55:56 +0100

    companies-information-card hover effect added

--- components/CompaniesWithInfo.js
+++ components/CompaniesWithInfo.js
@@ -46,6 +46,22 @@ export function CompanyInformationCard({
     setInfo(newInfo);
   }, [company]);
 
+  // Hover effect animation //
+  const handleOnMouseMove = (e) => {
+    const { currentTarget: target } = e;
+    const rect = target.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+  };
+
+  for (const card of document.querySelectorAll(".company-information-card")) {
+    card.onmousemove = (e) => handleOnMouseMove(e);
+  }
+  // Hover effect animation //
+
   return (
     <>
       {isOpen && (
